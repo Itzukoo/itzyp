02da0a0fb0a566d0794eaace1e9accd1ed660362
Itzukoo <162144343+Itzukoo@users.noreply.github.com>
2024-09-17 23:44:35 -0600

    Mi estado

--- mis-estados/src/App.js
+++ mis-estados/src/App.js
@@ -1,40 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
 
-import { PrimerComponente } from './PrimerComponente';
-import { SegundoComponente } from './SegundoComponente';
-import { TercerComponente } from './TercerComponente';
+import React from 'react';
+import MiEstado from './MiEstado'; // Importa el componente
 
-class App extends Component {
-  render() {
-
-
-    /**DEclaracion de objeto */
-    const ficha_medica = {
-      altura: "160cm",
-      grupoSangre: "B-",
-      estadoSalud: "Existiendooo",
-      alergias: "Chacales"
-    };
-
-
-    return (
-      <div className="App">
-        <div className="App-header">
-          <PrimerComponente />
-          <hr />
-        </div>
-
-        <SegundoComponente />
-        <hr />
-
-        <TercerComponente
-          nombre="Lucia"
-          apellido="Mondragon"
-          ficha= {ficha_medica} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Mi aplicación React</h1>
+      <MiEstado /> {/* Usa el componente aquí */}
+    </div>
+  );
+};
 
 export default App;
